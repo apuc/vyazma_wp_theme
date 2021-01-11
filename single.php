3a6f9dd891882b5bae2b1c3b490019a34ab0f852
Koronich <aleksadnrkorona@gmail.com>
2021-01-11 15:12:51 +0300

    fix formated text

--- single.php
+++ single.php
@@ -12,52 +12,58 @@ while (have_posts()) : the_post(); ?>
     <section>
         <div class="container_content1">
             <div class="container_content_section-content1">
-                <p class="section-content_svg"><img class="section-content_svg_calendar"
-                                                    src="<?= get_template_directory_uri() ?>/raw_html/img/calendar.svg"
-                                                    alt=""> <span> <?php the_date() ?> </span>
-                    <img class="section-content_svg_messege1"
-                         src="<?= get_template_directory_uri() ?>/raw_html/img/messege.svg" alt=""></p>
-                <h1><?php the_title() ?></h1>
+                <div class="container_content__article_img1">
+                    <div class="container_content__article_img1_wrapper">
+                        <img src="<?= $sp_obj->get_thumbnail(get_the_ID(), '') ?>" alt="">
+                    </div>
+                    <div class="apiVkComments1 transferJs">
+                        <h2>Последние коментарии</h2>
+                        <script type="text/javascript">
+                            VK.init({
+                                apiId: 7722333,
+                                onlyWidgets: true
+                            });
+                        </script>
+                        <div id="vk_comments"></div>
+                        <script type="text/javascript">
+                            VK.Widgets.Comments("vk_comments", {limit: 5, attach: false});
+                        </script>
 
-                <p class="container_content_section-content_description">
-                    <?= str_replace(array('<pre>', '</pre>'), '', get_the_content()) ?>
-                </p>
-                <div class="container_content_section-content__share-news1"><span class="share-news-title1"> Делитесь новостями</span>
-                    <div>
-                        <a href="" class="share-news1"><img
-                                    src="<?= get_template_directory_uri() ?>/raw_html/img/twitter-circled.svg"
-                                    alt=""></a>
-                        <a href="" class="share-news1"><img
-                                    src="<?= get_template_directory_uri() ?>/raw_html/img/facebook-circled.svg"
-                                    alt=""></a>
-                        <a href="" class="share-news1"><img
-                                    src="<?= get_template_directory_uri() ?>/raw_html/img/vkontakte-circled.svg"
-                                    alt=""></a>
                     </div>
                 </div>
+                <p class="section-content_svg">
+                    <img class="section-content_svg_calendar"
+                         src="<?= get_template_directory_uri() ?>/raw_html/img/calendar.svg"
+                         alt=""> <span> <?php the_date() ?> </span>
+                    <img class="section-content_svg_messege1"
+                         src="<?= get_template_directory_uri() ?>/raw_html/img/messege.svg" alt=""></p>
+
+                <h1><?php the_title() ?></h1>
+
+                <p class="container_content_section-content_description1">
+                    <?= str_replace(array('<pre>', '</pre>'), '', get_the_content()) ?>
+                </p>
+
+                <!-- Social share -->
+                <div class="container_content_section-content__share-news1">
+
+                    [widget id="zoom-social-icons-widget-2"]
+                    <!--
+                    <span class="share-news-title1"> Делитесь новостями</span>
+                    <div>
+                        <a href="" class="share-news1"><img
+                                    src="<?/*= get_template_directory_uri() */ ?>/raw_html/img/twitter-circled.svg"
+                                    alt=""></a>
+                        <a href="" class="share-news1"><img
+                                    src="<?/*= get_template_directory_uri() */ ?>/raw_html/img/facebook-circled.svg"
+                                    alt=""></a>
+                        <a href="" class="share-news1"><img
+                                    src="<?/*= get_template_directory_uri() */ ?>/raw_html/img/vkontakte-circled.svg"
+                                    alt=""></a>
+                    </div>-->
+                </div>
             </div>
 
-            <div class="container_content__article_img1">
-                <div class="container_content__article_img1_wrapper">
-                    <img src="<?= $sp_obj->get_thumbnail(get_the_ID(), '') ?>" alt="">
-                </div>
-                <div class="apiVkComments1 transferJs">
-                    <h2>Последние коментарии</h2>
-                    <!--                                            <img src="-->
-                    <!--                    --><?//= get_template_directory_uri() ?><!--/imgs/vk_widget.png" alt="">-->
-                    <script type="text/javascript">
-                        VK.init({
-                            apiId: 7722333,
-                            onlyWidgets: true
-                        });
-                    </script>
-                    <div id="vk_comments"></div>
-                    <script type="text/javascript">
-                        VK.Widgets.Comments("vk_comments", {limit: 5, attach: false});
-                    </script>
-
-                </div>
-            </div>
 
         </div>
     </section>

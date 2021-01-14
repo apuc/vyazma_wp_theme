e2bb792ae353750e6f55f69341d6cfbae0d5f5d6
Koronich <aleksadnrkorona@gmail.com>
2021-01-14 10:46:48 +0300

    replace static img w plugin

--- page.php
+++ page.php
@@ -12,34 +12,25 @@ while (have_posts()) : the_post(); ?>
         <section>
             <div class="container_content1">
                 <div class="container_content_section-content1">
-                    <p class="section-content_svg"><img class="section-content_svg_calendar"
-                                                        src="<?= get_template_directory_uri() ?>/raw_html/img/calendar.svg"
-                                                        alt=""> <span> <?php the_date() ?> </span>
-                        <img class="section-content_svg_messege1"
-                             src="<?= get_template_directory_uri() ?>/raw_html/img/messege.svg" alt=""></p>
+                    <p class="section-content_svg">
+                        <img class="section-content_svg_calendar"
+                             src="<?= get_template_directory_uri() ?>/raw_html/img/calendar.svg"
+                             alt=""> <span> <?php the_date() ?> </span>
+
                     <h1><?php the_title() ?></h1>
 
                     <p class="container_content_section-content_description">
                         <?= get_the_content() ?>
                     </p>
-                    <div class="container_content_section-content__share-news1"><span class="share-news-title1"> Делитесь новостями</span>
-                        <div>
-                            <a href="" class="share-news1"><img
-                                        src="<?= get_template_directory_uri() ?>/raw_html/img/twitter-circled.svg"
-                                        alt=""></a>
-                            <a href="" class="share-news1"><img
-                                        src="<?= get_template_directory_uri() ?>/raw_html/img/facebook-circled.svg"
-                                        alt=""></a>
-                            <a href="" class="share-news1"><img
-                                        src="<?= get_template_directory_uri() ?>/raw_html/img/vkontakte-circled.svg"
-                                        alt=""></a>
-                        </div>
+                    <div class="container_content_section-content__share-news1">
+                        <span class="share-news-title1"> Делитесь новостями</span>
+                        <?php echo do_shortcode('[widget id="zoom-social-icons-widget-2"]'); ?>
                     </div>
                 </div>
 
 
             </div>
-            <?php get_VK_comments_widget();?>
+            <?php get_VK_comments_widget(); ?>
 
         </section>
     </div>

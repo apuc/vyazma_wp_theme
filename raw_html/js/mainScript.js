646a6dce2100c8b86d393f46b5aa9390d61a7a65
Koronich <aleksadnrkorona@gmail.com>
2021-01-18 18:45:44 +0300

    visual changes

--- raw_html/js/mainScript.js
+++ raw_html/js/mainScript.js
@@ -33,48 +33,36 @@ window.addEventListener("resize", function () {
 	transferEl(vk, containerFirst, null , 937);
 })
 
-window.onload = function () {
+/*window.onload = function () {
 
-	/*
-		if (document.querySelector("#addOldArticle")) {
-			document.querySelector("#addOldArticle").addEventListener("click", function () {
-				let linkImg = "./img/vgrom.com_1453716065_oboi.jpg";
-				let linkTitle = "Название статьи Название статьи";
-				let articleText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mi";
-				let template = {
-					articleOld: `
-							<article class="oldSingleArticle  ">
-							<a href="" class="ArticleHref"></a>
-								<img class="oldArticleImg" src="{linkImg}" alt="">
-			<div>
-			<a href="" class="oldSingleArticle_title">{linkTitle}</a>
-			<p class="oldSingleArticle_description">{articleText}</p>
-			</div>
-			</article>`
-				};
-
-				let onceOldArticleGenerate = template.articleOld
-					.replace("{linkImg}", linkImg)
-					.replace("{linkTitle}", linkTitle)
-					.replace("{articleText}", articleText);
-
-				let areaInstall = document.querySelector(".add_OlderArticle");
-				areaInstall.innerHTML += onceOldArtieGenerate;
-			});
-		}
-
-		/*
-			const iframeVkApi = document.querySelector("#vkwidget1");
-			console.log(iframeVkApi);
-			const iframeDocument = iframeVkApi.contentWindow;
-			console.log(iframeDocument);
-			let iframeVkApiKek = async function (iframeVkApi) {
-				await console.log(iframeVkApi);
+	if (document.querySelector("#addOldArticle")) {
+		document.querySelector("#addOldArticle").addEventListener("click", function () {
+			let linkImg = "./img/vgrom.com_1453716065_oboi.jpg";
+			let linkTitle = "Название статьи Название статьи";
+			let articleText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mi";
+			let template = {
+				articleOld: `
+						<article class="oldSingleArticle  ">
+						<a href="" class="ArticleHref"></a>
+							<img class="oldArticleImg" src="{linkImg}" alt="">
+		<div>
+		<a href="" class="oldSingleArticle_title">{linkTitle}</a>
+		<p class="oldSingleArticle_description">{articleText}</p>
+		</div>
+		</article>`
 			};
-			iframeVkApiKek(iframeVkApi);
-		*/
+
+			let onceOldArticleGenerate = template.articleOld
+				.replace("{linkImg}", linkImg)
+				.replace("{linkTitle}", linkTitle)
+				.replace("{articleText}", articleText);
+
+			let areaInstall = document.querySelector(".add_OlderArticle");
+			areaInstall.innerHTML += onceOldArticleGenerate;
+		});
+	}
 
 
-};
+};*/
 
 
